51276ba24d26c1af7e2c7e6684deaa31bd7ac29a
Carlos Antonio Pérez Acosta <00098925@uca.edu.sv>
2026-09-09 14:52:55 -0600

    Se agrego el consultar el saldo inicial

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
 
+void mostrarSaldo(double saldo){
+    cout << "Saldo actual: "<< saldo<< endl;
+}
+
 int main()
 {
     int opcion;
+    double saldo = 1000
+    ;
     do
     {
         cout << "------------------ Menu de cajero ------------------------" << endl;
         cout << "--------------- 1. Consultar saldo -----------------------" << endl;
         cout << "--------------- 2. Retirar dinero ------------------------" << endl;
         cout << "--------------- 3. Depositar dinero ----------------------" << endl;
-        cout << "--------------- 4. Salir ---------------------------------" << endl<< endl;
+        cout << "--------------- 4. Salir ---------------------------------" << endl
+             << endl;
 
         cout << "Ingrese una opcion: ";
         cin >> opcion;
@@ -20,6 +27,7 @@ int main()
         {
         case 1:
             cout << "Consultar saldo" << endl;
+            mostrarSaldo(saldo);
             break;
         case 2:
             cout << "Retirar dinero" << endl;
